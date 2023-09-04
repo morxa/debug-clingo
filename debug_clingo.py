9d048c09499ff2bfdc10edd384b125394991dc6e
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2023-09-04 20:58:42 +0200

    Extend script so it can be used with multiple compute nodes

--- debug_clingo.py
+++ debug_clingo.py
@@ -50,9 +50,12 @@ def parse_files(files):
     return other, constraints
 
 
-def debug_files(files, check_full_problem=True):
+def get_num_steps(files):
     other, constraints = parse_files(files)
-    # Check if the problem is completely satisfiable.
+    return len(constraints)
+
+
+def check_full_problem(other, constraints):
     ctl = clingo.Control()
     if check_full_problem:
         log.info("Checking whether complete program is satisfiable")
@@ -61,43 +64,69 @@ def debug_files(files, check_full_problem=True):
         ctl.add("base", [], program)
         ctl.ground([("base", [])])
         res = ctl.solve(on_model=on_model)
-        if res.satisfiable:
-            log.info("Complete problem is satisfiable, nothing to debug!")
-            return
-    # Check if there is a single constraint that results in UNSAT.
+        return res.satisfiable
+
+
+def debug_program(other, constraints):
     found = False
     for i in range(len(constraints)):
-        log.debug(f'Checking constraint {i}:\n{constraints[i]}')
-        ctl = clingo.Control()
-        program = "\n".join(other + constraints[:i] + constraints[i + 1:])
-        ctl.add("base", [], program)
-        ctl.ground([("base", [])])
-        res = ctl.solve(on_model=on_model)
-        if res.satisfiable:
+        step = i + 1
+        if debug_step(other, constraints, step):
             found = True
-            log.info(f"Constraint {i} is unsatisfiable:\n{constraints[i]}")
     if not found:
         log.info(
             "Removing a single constraint does not make the problem satisfiable, giving up!"
         )
 
 
+def debug_step(other, constraints, step):
+    log.debug(f'Checking constraint {step}: {constraints[step]}')
+    ctl = clingo.Control()
+    program = "\n".join(other + constraints[:step - 1] + constraints[step:])
+    ctl.add("base", [], program)
+    ctl.ground([("base", [])])
+    res = ctl.solve(on_model=on_model)
+    if res.satisfiable:
+        log.info(f"Constraint {step} is unsatisfiable: {constraints[step]}")
+    return res.satisfiable
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', help='The file(s) to debug', nargs='+')
+    parser.add_argument('files', help='The file(s) to debug', nargs='+')
     parser.add_argument('-v',
                         '--verbose',
                         action='store_true',
                         help='Verbose output')
     parser.add_argument(
         '-s',
+        '--step',
+        type=int,
+        help='Only run one specific step (remove constraint i); 1-indexed')
+    parser.add_argument('--get-num-steps',
+                        action='store_true',
+                        help='Get the number of steps')
+    parser.add_argument(
+        '-n',
         '--skip-full-problem',
         action='store_false',
         dest='check_full_problem',
         help='Skip checking whether the full problem is satisfiable')
     args = parser.parse_args()
     log.setLevel(logging.DEBUG if args.verbose else logging.INFO)
-    debug_files(args.file, args.check_full_problem)
+    if args.get_num_steps:
+        log.setLevel(logging.ERROR)
+        print(get_num_steps(args.files))
+        return
+    other, constraints = parse_files(args.files)
+    if args.check_full_problem:
+        if check_full_problem(other, constraints):
+            log.info("Complete problem is satisfiable, nothing to debug!")
+            return
+    if args.step:
+        debug_step(other, constraints, args.step)
+    else:
+        debug_program(other, constraints)
 
 
 if __name__ == '__main__':
